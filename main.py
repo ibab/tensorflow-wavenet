3a04b51201fde47c6f026bca5a63f0cd2d96697c
Igor Babuschkin <igor@babuschk.in>
2016-09-13 15:07:00 +0100

    Clean up code

---
+++ main.py
@@ -0,0 +1,96 @@
+
+import glob
+import re
+
+import threading
+import tensorflow as tf
+from tensorflow.contrib import ffmpeg
+
+from wavenet import WaveNet
+
+BATCH_SIZE = 1
+CHANNELS = 256
+DATA_DIRECTORY='./VCTK-Corpus'
+
+def create_vctk_inputs(directory):
+    # TODO make sure that text is matched correctly to the samples
+
+    # We retrieve each audio sample, the corresponding text, and the speaker id
+    audio_filenames = glob.glob(directory + '/wav48/**/*.wav', recursive=True)
+
+    # Mysteriously, speaker 315 doesn't have any text files associated with them
+    audio_filenames = list(filter(lambda x: not '315' in x, audio_filenames))
+    text_filenames = [f.replace('wav48', 'txt').replace('.wav', '.txt')
+                        for f in audio_filenames]
+
+    # Find the speaker ID and map it into a range [0, ...,  num_speakers]
+    dirs = glob.glob(directory + '/txt/p*')
+    SPEAKER_RE = r'p([0-9]+)'
+    ids = [re.findall(SPEAKER_RE, d)[0] for d in dirs]
+    speaker_map = {speaker_id: idx for idx, speaker_id in enumerate(ids)}
+    speaker = [speaker_map[re.findall(SPEAKER_RE, p)[0]] for p in text_filenames]
+
+    audio_files = tf.train.string_input_producer(audio_filenames)
+    txt_files = tf.train.string_input_producer(text_filenames)
+    speaker_values = tf.train.input_producer(speaker)
+
+    reader_a = tf.WholeFileReader()
+    _, audio_values = reader_a.read(audio_files)
+    reader_b = tf.WholeFileReader()
+    _, txt_values = reader_a.read(txt_files)
+
+    waveform = ffmpeg.decode_audio(
+        audio_values,
+        file_format='wav',
+        # Downsample to 16khz
+        samples_per_second=1<<13,
+        # Corpus uses mono
+        channel_count=1)
+
+    return waveform, txt_values, speaker_values.dequeue()
+
+def main():
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
+    # Load raw waveform from VCTK corpus
+    with tf.name_scope('create_inputs'):
+        audio, txt, speaker = create_vctk_inputs(DATA_DIRECTORY)
+
+    queue = tf.PaddingFIFOQueue(
+        256,
+        ["float32", "string", "int32"],
+        shapes=[(None, 1), (), ()])
+    enqueue = queue.enqueue([audio, txt, speaker])
+    audio_batch, _, _ = queue.dequeue_many(BATCH_SIZE)
+
+    # Create network
+    dilations = [1, 2, 4, 8, 16, 32]
+    net = WaveNet(BATCH_SIZE, CHANNELS, dilations)
+    loss = net.loss(audio_batch)
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.10)
+    trainable = tf.trainable_variables()
+    optim = optimizer.minimize(loss, var_list=trainable)
+
+    # Set up logging for TensorBoard
+    writer = tf.train.SummaryWriter('./logdir')
+    writer.add_graph(tf.get_default_graph())
+    summaries = tf.merge_all_summaries()
+
+    init = tf.initialize_all_variables()
+    sess.run(init)
+
+    # Enqueue examples in the background
+    coord = tf.train.Coordinator()
+    qr = tf.train.QueueRunner(queue, [enqueue])
+    qr.create_threads(sess, coord=coord, start=True)
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    for i in range(1000):
+        summary, loss_value, _ = sess.run([summaries, loss, optim])
+        writer.add_summary(summary, i)
+        print('Loss: {}'.format(loss_value))
+
+    coord.request_stop()
+    coord.join(threads)
+
+if __name__ == '__main__':
+    main()
---
+++ wavenet.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+
+class WaveNet(object):
+
+    def __init__(self, batch_size, channels, dilations):
+        self.dilations = []
+        self.batch_size = batch_size
+        self.channels = channels
+        self.dilations = dilations
+
+    def _create_dilation_layer(self, input_batch, i, dilation):
+        wf = tf.Variable(tf.truncated_normal([1, 2, 256, 256], stddev=0.2, name="filter"))
+        wg = tf.Variable(tf.truncated_normal([1, 2, 256, 256], stddev=0.2, name="gate"))
+
+        # TensorFlow has an operator for convolution with holes
+        tmp1 = tf.nn.atrous_conv2d(input_batch, wf,
+                rate=dilation,
+                padding="SAME",
+                name="conv_f")
+        tmp2 = tf.nn.atrous_conv2d(input_batch, wg,
+                rate=dilation,
+                padding="SAME",
+                name="conv_g")
+
+        out = tf.tanh(tmp1) * tf.sigmoid(tmp2)
+
+        # Shift output to the right by dilation count so that only current/past
+        # values can influence the prediction
+        out = tf.slice(out, [0, 0, 0, 0], [-1, -1, tf.shape(out)[2] - dilation, -1])
+        out = tf.pad(out, [[0, 0], [0, 0], [dilation, 0], [0, 0]])
+
+        w = tf.Variable(tf.truncated_normal([1, 1, 256, 256], stddev=0.20, name="dense"))
+        transformed = tf.nn.conv2d(out, w, strides=[1, 1, 1, 1], padding="SAME", name="dense")
+
+        tf.histogram_summary('layer{}_filter'.format(i), wf)
+        tf.histogram_summary('layer{}_guard'.format(i), wg)
+        tf.histogram_summary('layer{}_weights'.format(i), w)
+
+        return input_batch + transformed
+
+    def _preprocess(self, audio):
+        '''Quantize waveform amplitudes
+        '''
+        mu = self.channels - 1
+        # Perform mu-law companding transformation (ITU-T, 1988)
+        signal = tf.sign(audio) * (tf.log(1 + mu * tf.abs(audio))
+                                    / tf.log(1. + mu))
+        quantized = tf.cast((signal + 1) / 2 * mu, tf.int32)
+
+        return quantized
+
+    def _create_network(self, input_batch):
+        outputs = []
+        current_layer = input_batch
+
+        # Add all defined dilation layers
+        for i, dilation in enumerate(self.dilations):
+            with tf.name_scope('layer{}'.format(i)):
+                current_layer = self._create_dilation_layer(
+                        current_layer,
+                        i,
+                        dilation=dilation)
+                outputs.append(current_layer)
+
+        with tf.name_scope('postprocess'):
+            # Some 1x1 convolution + ReLU layers to postprocess the output
+            w1 = tf.Variable(tf.truncated_normal([1, 1, 256, 256], stddev=0.3, name="postprocess1"))
+            w2 = tf.Variable(tf.truncated_normal([1, 1, 256, 256], stddev=0.3, name="postprocess2"))
+
+            tf.histogram_summary('postprocess1_weights', w1)
+            tf.histogram_summary('postprocess2_weights', w2)
+
+            # We skip connections from the output of each layers, adding them all up here
+            summed = tf.add_n(outputs)
+            transformed1 = tf.nn.relu(summed)
+            conv1 = tf.nn.conv2d(transformed1, w1, [1, 1, 1, 1], padding="SAME")
+            transformed2 = tf.nn.relu(conv1)
+            conv2 = tf.nn.conv2d(transformed2, w2, [1, 1, 1, 1], padding="SAME")
+
+        return conv2
+
+    def loss(self, input_batch):
+        # One-hot encode waveform amplitudes, so we can define the network as a
+        # categorical distribution over possible amplitudes
+
+        input_batch = self._preprocess(input_batch)
+
+        with tf.name_scope('one_hot_encode'):
+            waves = tf.reshape(input_batch, [self.batch_size, 1, -1])
+            encoded = tf.one_hot(input_batch, depth=self.channels, dtype=tf.float32)
+            encoded = tf.reshape(encoded, [self.batch_size, 1, -1, self.channels])
+
+        raw_output = self._create_network(encoded)
+
+        with tf.name_scope('loss'):
+            # Shift original input left by one sample, which means that each
+            # output pixel has to predict the next input pixel
+            shifted = tf.slice(encoded, [0, 0, 1, 0], [-1, -1, tf.shape(encoded)[2] - 1, -1])
+            shifted = tf.pad(shifted, [[0, 0], [0, 0], [0, 1], [0, 0]])
+
+            loss = tf.nn.softmax_cross_entropy_with_logits(raw_output, tf.reshape(shifted, [-1, self.channels]))
+            reduced_loss =  tf.reduce_mean(loss)
+
+            tf.scalar_summary('loss', reduced_loss)
+
+        return reduced_loss
