c79add15433c65ab7d44c3ba052a378b95fbc0f2
Gene Kogan <kogan.gene@gmail.com>
2016-09-19 01:39:26 +0200

    threaded audio loader with librosa (#38)
    * custom runner for threaded loading audio
    
    * switched to batch size from args
    
    * fixed indentation bug
    
    * fixed shape mismatching issues
    
    * synced with upstream changes, commited to main training file
    
    * removed temp training file
    
    * put back coordinator

--- train.py
+++ train.py
@@ -13,6 +13,10 @@ import os
 import re
 import sys
 import time
+import threading
+import numpy as np
+import fnmatch
+import librosa
 
 import tensorflow as tf
 from tensorflow.contrib import ffmpeg
@@ -26,6 +30,7 @@ LOGDIR = './logdir'
 NUM_STEPS = 4000
 LEARNING_RATE = 0.02
 WAVENET_PARAMS = './wavenet_params.json'
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='WaveNet example network')
@@ -49,39 +54,67 @@ def get_arguments():
     return parser.parse_args()
 
 
-def create_vctk_inputs(directory, sample_rate=16000):
-    '''Loads audio samples and speaker IDs from the VCTK dataset.'''
+def scan_directory(directory):
+    files = []
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in fnmatch.filter(filenames, '*.wav'):
+            files.append(os.path.join(root, filename))
+    return files
 
-    # We retrieve each audio sample, the corresponding text, and the speaker id
-    audio_glob = directory + '/wav48/*/*.wav'
-    audio_filenames = glob.glob(audio_glob)
-    if len(audio_filenames) == 0:
-        print('No audio files matching {} have been found!'.format(audio_glob))
-        sys.exit(1)
 
-    print('Found {} audio files'.format(len(audio_filenames)))
-    audio_files = tf.train.string_input_producer(audio_filenames)
-    reader = tf.WholeFileReader()
-    _, audio_values = reader.read(audio_files)
+def iterate_through_directory(directory, sample_rate):
+    files = scan_directory(directory)
+    for f in files:
+        audio, sr = librosa.load(f, sr=sample_rate, mono=True)
+        audio = audio.reshape(-1, 1)
+        yield audio, 0
 
-    # Find the speaker ID and map it into a range [0, ...,  num_speakers].
-    dirs = glob.glob(directory + '/wav48/p*')
-    speaker_re = re.compile(r'p([0-9]+)')
-    ids = [speaker_re.findall(d)[0] for d in dirs]
-    speaker_map = {speaker_id: idx for idx, speaker_id in enumerate(ids)}
-    speaker = [speaker_map[speaker_re.findall(p)[0]] for p in audio_filenames]
 
-    audio_files = tf.train.string_input_producer(audio_filenames)
-    speaker_values = tf.train.input_producer(speaker)
+def iterate_through_vctk(directory, sample_rate):
+    files = scan_directory(directory)
+    speaker_re = re.compile(r'p([0-9]+)_([0-9]+)\.wav')
+    for j,f in enumerate(files):
+        audio, sr = librosa.load(f, sr=sample_rate, mono=True)
+        audio = audio.reshape(-1, 1)
+        speaker_id, recording_id = [int(i) for i in speaker_re.findall(f)[0]]
+        yield audio, speaker_id
 
-    waveform = ffmpeg.decode_audio(
-        audio_values,
-        file_format='wav',
-        samples_per_second=sample_rate,
-        # Corpus uses mono.
-        channel_count=1)
 
-    return waveform, speaker_values.dequeue()
+class CustomRunner(object):
+    def __init__(self, args, wavenet_params, coord):
+        self.args = args
+        self.wavenet_params = wavenet_params
+        self.coord = coord
+        self.threads = []
+        self.dataX = tf.placeholder(dtype=tf.float32, shape=None)
+        self.dataY = tf.placeholder(dtype=tf.int32)
+        self.queue = tf.PaddingFIFOQueue(
+            256,  # Queue size.
+            ['float32', 'int32'],
+            shapes=[(None, 1), ()])
+        self.enqueue = self.queue.enqueue([self.dataX, self.dataY])
+
+    def get_inputs(self):
+        self.audio_batch, _ = self.queue.dequeue_many(self.args.batch_size)
+        return self.audio_batch, _
+
+    def thread_main(self, sess):
+        for audio, label in iterate_through_vctk(self.args.data_dir, self.wavenet_params["sample_rate"]):
+            if self.coord.should_stop():
+                self.stop_threads()
+            sess.run(self.enqueue, feed_dict={self.dataX:audio, self.dataY:label})
+
+    def stop_threads():
+        for t in self.threads:
+            t.stop()
+
+    def start_threads(self, sess, n_threads=1):
+        for n in range(n_threads):
+            t = threading.Thread(target=self.thread_main, args=(sess,))
+            t.daemon = True # thread will close when parent quits
+            t.start()
+            self.threads.append(t)
+        return self.threads
 
 
 def main():
@@ -92,20 +125,14 @@ def main():
     with open(args.wavenet_params, 'r') as f:
         wavenet_params = json.load(f)
 
-    sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
+    # create coordinator
+    coord = tf.train.Coordinator()
+    
     # Load raw waveform from VCTK corpus.
     with tf.name_scope('create_inputs'):
-        audio, speaker = create_vctk_inputs(args.data_dir,
-                                            wavenet_params["sample_rate"])
-
-        queue = tf.PaddingFIFOQueue(
-            256,  # Queue size.
-            ['float32', 'int32'],
-            shapes=[(None, 1), ()])
-        enqueue = queue.enqueue([audio, speaker])
-        # Don't condition on speaker IDs for now.
-        audio_batch, _ = queue.dequeue_many(args.batch_size)
-
+        custom_runner = CustomRunner(args, wavenet_params, coord)
+        audio_batch, _ = custom_runner.get_inputs()
+    
     # Create network.
     net = WaveNet(args.batch_size,
                   wavenet_params["quantization_steps"],
@@ -124,51 +151,49 @@ def main():
     run_metadata = tf.RunMetadata()
     summaries = tf.merge_all_summaries()
 
+    # Set up session
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))
     init = tf.initialize_all_variables()
     sess.run(init)
-
-    # Load and enqueue examples in the background.
-    coord = tf.train.Coordinator()
-    qr = tf.train.QueueRunner(queue, [enqueue])
-    qr.create_threads(sess, coord=coord, start=True)
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    custom_runner.start_threads(sess)
 
     # Saver for storing checkpoints of the model.
     saver = tf.train.Saver()
 
     try:
-      for step in range(args.num_steps):
-          start_time = time.time()
-          if args.store_metadata and step % 50 == 0:
-              # Slow run that stores extra information for debugging.
-              print('Storing metadata')
-              run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-              summary, loss_value, _ = sess.run(
-                  [summaries, loss, optim],
-                  options=run_options,
-                  run_metadata=run_metadata)
-              writer.add_summary(summary, step)
-              writer.add_run_metadata(run_metadata, 'step_{:04d}'.format(step))
-              tl = timeline.Timeline(run_metadata.step_stats)
-              timeline_path = os.path.join(logdir, 'timeline.trace')
-              with open(timeline_path, 'w') as f:
-                  f.write(tl.generate_chrome_trace_format(show_memory=True))
-          else:
-              summary, loss_value, _ = sess.run([summaries, loss, optim])
-              writer.add_summary(summary, step)
+        for step in range(args.num_steps):
+            start_time = time.time()
+            if args.store_metadata and step % 50 == 0:
+                # Slow run that stores extra information for debugging.
+                print('Storing metadata')
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                summary, loss_value, _ = sess.run(
+                    [summaries, loss, optim],
+                    options=run_options,
+                    run_metadata=run_metadata)
+                writer.add_summary(summary, step)
+                writer.add_run_metadata(run_metadata, 'step_{:04d}'.format(step))
+                tl = timeline.Timeline(run_metadata.step_stats)
+                timeline_path = os.path.join(logdir, 'timeline.trace')
+                with open(timeline_path, 'w') as f:
+                    f.write(tl.generate_chrome_trace_format(show_memory=True))
+            else:
+                summary, loss_value, _ = sess.run([summaries, loss, optim])
+                writer.add_summary(summary, step)
 
-          duration = time.time() - start_time
-          print('step %d - loss = %.3f, (%.3f sec/step)' % (step, loss_value, duration))
+            duration = time.time() - start_time
+            print('step %d - loss = %.3f, (%.3f sec/step)' % (step, loss_value, duration))
 
-          if step % 50 == 0 or step == args.num_steps - 1:
-              checkpoint_path = os.path.join(logdir, 'model.ckpt')
-              print('Storing checkpoint to {}'.format(checkpoint_path))
-              saver.save(sess, checkpoint_path, global_step=step)
+            if step % 50 == 0:
+                checkpoint_path = os.path.join(logdir, 'model.ckpt')
+                print('Storing checkpoint to {}'.format(checkpoint_path))
+                saver.save(sess, checkpoint_path, global_step=step)
 
     finally:
-      coord.request_stop()
-      coord.join(threads)
-
+        coord.request_stop()
+        coord.join(threads)
+        
 
 if __name__ == '__main__':
     main()
