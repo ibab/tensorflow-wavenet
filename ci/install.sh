0aa79141b0a96a5a3e2d9ba94428f4d7ac5850cc
Igor Babuschkin <igor@babuschk.in>
2016-09-22 15:50:50 +0200

    Pick correct TensorFlow package on travis

--- ci/install.sh
+++ ci/install.sh
@@ -11,5 +11,8 @@ source activate test
 pip install nose
 pip install librosa
 
-pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-$TENSORFLOW-cp35-cp35m-linux_x86_64.whl
-
+if [[ $TRAVIS_PYTHON_VERSION == "2.7" ]]; then
+    pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-$TENSORFLOW-cp27-none-linux_x86_64.whl
+else
+    pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-$TENSORFLOW-cp35-cp35m-linux_x86_64.whl
+fi
