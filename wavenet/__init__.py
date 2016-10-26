a202b0eb194e29829a0603fe3abf6a323173beb2
Samuel Graván <zeta27almacen@gmail.com>
2016-10-26 21:53:45 +0100

    Update __init__.py

--- wavenet/__init__.py
+++ wavenet/__init__.py
@@ -2,9 +2,6 @@ from .model import WaveNetModel
 from .audio_reader import AudioReader
 from .text_reader import TextReader
 from .image_reader import ImageReader
-import audio_reader
-import text_reader
-import image_reader
 from .ops import (FileReader, mu_law_encode, mu_law_decode, time_to_batch,
                   batch_to_time, causal_conv, optimizer_factory, write_output,
                   create_seed_audio)
