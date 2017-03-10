Fix log() numerical instabilities (#240)
* Fix numerical instability in test_mu_law.py

See the warning on #238.

* Update ops.py

* Update ops.py

wavenet/ops.py
@@ -64,15 +64,15 @@ def causal_conv(value, filter_, dilation, name='causal_conv'):
 def mu_law_encode(audio, quantization_channels):
     '''Quantizes waveform amplitudes.'''
     with tf.name_scope('encode'):
-        mu = quantization_channels - 1
+        mu = tf.to_float(quantization_channels - 1)
         # Perform mu-law companding transformation (ITU-T, 1988).
         # Minimum operation is here to deal with rare large amplitudes caused
         # by resampling.
         safe_audio_abs = tf.minimum(tf.abs(audio), 1.0)
-        magnitude = tf.log(1 + mu * safe_audio_abs) / tf.log(1. + mu)
+        magnitude = tf.log1p(mu * safe_audio_abs) / tf.log1p(mu)
         signal = tf.sign(audio) * magnitude
         # Quantize signal to the specified number of levels.
-        return tf.cast((signal + 1) / 2 * mu + 0.5, tf.int32)
+        return tf.to_int32((signal + 1) / 2 * mu + 0.5)
 
 
 def mu_law_decode(output, quantization_channels):
@@ -80,8 +80,7 @@ def mu_law_decode(output, quantization_channels):
     with tf.name_scope('decode'):
         mu = quantization_channels - 1
         # Map values back to [-1, 1].
-        casted = tf.cast(output, tf.float32)
-        signal = 2 * (casted / mu) - 1
+        signal = 2 * (tf.to_float(output) / mu) - 1
         # Perform inverse of mu-law transformation.
         magnitude = (1 / mu) * ((1 + mu)**abs(signal) - 1)
         return tf.sign(signal) * magnitude