b52ccb7166086249f4088e117572b0bdf1cf8e48
Abolfazl Shahbazi <abolfazl.shahbazi@intel.com>
2020-10-03 22:16:29 -0700

    Fix 'Travis CI' build failures

--- ci/install.sh
+++ ci/install.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+wget --no-check-certificate \
+    https://repo.anaconda.com/miniconda/Miniconda2-4.7.12.1-Linux-x86_64.sh -O miniconda.sh
+
+chmod +x miniconda.sh && ./miniconda.sh -b -p ${HOME}/miniconda
+
+export PATH="${HOME}/miniconda/bin:/usr/lib/llvm-7/bin:${PATH}"
+
 conda config --set always_yes yes --set changeps1 no
-conda update -q conda
 
-conda create -q -n test python=$TRAVIS_PYTHON_VERSION numpy scipy
-source activate test
-pip install -r requirements_test.txt
+conda create -q -n test${TRAVIS_PYTHON_VERSION} python=${TRAVIS_PYTHON_VERSION}
+source activate test${TRAVIS_PYTHON_VERSION}
+
+python -m pip install pip==19.3.1
+
+python -m pip install -r requirements_test.txt
